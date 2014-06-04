5d6461b499bdd110d82f2cc5280be09d54d90979
tmroz <tom.mroz@d214.org>
2014-06-04 13:27:51 -0500

    added delete method to course

--- GradeMachine/ViewController.m
+++ GradeMachine/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Course.h"
 
 @interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *myCourseTableView;
@@ -18,22 +19,63 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    Course *course1 = [[Course alloc] initWithTitle:@"Test Course" andPeriod:@"Period 1"];
+    self.courseList =[NSMutableArray arrayWithObjects: course1, nil];
+    self.navigationItem.rightBarButtonItem = self.editButtonItem;
 
-    
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return self.courseList.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    Course *selectedCourse= [self.courseList objectAtIndex:indexPath.row];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"courseCellID"];
-
+    cell.textLabel.text = selectedCourse.courseTitle;
 
     return cell;
 }
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        // Delete the row from the data source
+        [self.courseList removeObjectAtIndex:indexPath.row];
+        [tableView reloadData];
+
+    }
+    else if (editingStyle == UITableViewCellEditingStyleInsert)
+    {
+
+    }
+
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    /**if (indexPath.section == 0) {
+        if (indexPath.row == 0) {
+            return NO;
+        }
+    }*/
+    return YES;
+    
+}
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+    [super setEditing:editing animated:animated];
+    [self.myCourseTableView setEditing:editing animated:YES];
+    if (editing) {
+        //addButton.enabled = NO;
+    } else {
+        //addButton.enabled = YES;
+    }
+}
+
 
 @end
